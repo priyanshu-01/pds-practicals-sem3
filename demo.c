3d205f4321f1b6f0bd332a511a1a1877a9cb79c4
Priyanshu Agarwal <priyanshu.agarwal88@gmail.com>
2020-12-21 23:14:31 +0530

    added some code

---
+++ demo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct Account
+{
+    char name[20];
+    int number;
+    int balance;
+};
+
+int main()
+{
+    struct Account myStruct;
+    printf("enter details of account %d: \n", 1);
+
+    printf("enter name : ");
+    scanf("%s", myStruct.name);
+
+    printf("enter number :");
+    scanf("%d", myStruct.number);
+
+    printf("enter balance :");
+    scanf("%d", myStruct.balance);
+
+    printf("details entered are : \n");
+    printf("name : %s \n", myStruct.name);
+    printf("number : %d \n", myStruct.number);
+    printf("balance : %d \n", myStruct.balance);
+}
--- hello.c
+++ hello.c
@@ -1,7 +1,48 @@
-#include <stdio.h> 
- 
-int main() 
-{ 
-    puts("hello world"); 
-    return 0; 
+#include <stdio.h>
+
+struct Account
+{
+    char name[20];
+    int number;
+    float balance;
+};
+
+int main()
+{
+    int a;
+    char str[20];
+    struct Account arr[2];
+    // printf("null is %d", arr[2] );
+
+    // printf("enter a no. \n");
+    // scanf("%d", &a);
+    // printf("entered number is %d \n",a);
+    // printf("enter a string : \n");
+    // scanf("%s",str);
+    // printf("entered string is %s", str);
+
+        // printf("enter number :");
+        // scanf("%d", arr[0].number);
+
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("enter details of account %d: \n", i);
+
+        // printf("enter name : ");
+        // scanf("%s", arr[i].name);
+
+        printf("enter number :");
+        scanf("%d", arr[i].number);
+
+        printf("enter balance :");
+        scanf("%f", arr[i].balance);
+    }
+    printf("details entered are : \n");
+    for (int i = 0; i < 2; i++)
+    {
+        printf("name : %s \n", arr[i].name);
+        printf("number : %d \n", arr[i].number);
+        printf("balance : %f \n", arr[i].balance);
+    }
 }
